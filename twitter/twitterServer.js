30f35d1b0759c488c304a4c324f6ebee82c411b8
Eric Githinji <ericgithinji@gmail.com>
2022-12-27 12:37:56 +0300

    Add tweet processing functionality. Improve handling of disconnects.

--- twitter/twitterServer.js
+++ twitter/twitterServer.js
@@ -1,81 +1,147 @@
 const https = require('https');
-const { addRules } = require('./addRules');
-const { delRules } = require('./delRules');
+const AreaDesc = require('../models/areaDesc');
+const Geometry = require('../models/objects/geometry');
+const Properties = require('../models/objects/properties');
+const Polygon = require('../models/polygon');
+const Feature = require('../models/feature');
+const { search_tweet, random_point } = require('../rust-modules/pkg');
 require('dotenv').config();
 
-let lastKeepAlive = new Date();
-let httpTimeToWait = 60000;
-let tcpTimeToWait = 0;
-
-addRules();
-delRules();
-
-const options = {
-    hostname: 'api.twitter.com',
-    port: 443,
-    path: '/2/tweets/search/stream',
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer '+process.env.TWITTER_BEARER_TOKEN, 'Content-Type': 'application/json' },
-};
-
-setInterval(() => {
-    let now = new Date();
-    let elapsed = Math.abs((now.getTime() - lastKeepAlive.getTime())/1000) ;
-    if (elapsed > 60) {
-        console.log('keep alive time elapsed: destroying connection')
-        // destroying the connection will triggers an 'error' event
-        // either  on the response object or on the request object
-        https.globalAgent.destroy();
-    }
-}, 30000);
-
-(function connectToStream() {
-    req = https.get(options, (res) => {
-        console.log('statusCode:', res.statusCode);
-        if (res.statusCode !== 200) {
-            console.log(new Date() + "http level error: restart connection");
-            setTimeout(() => {
-                connectToStream();
-                httpTimeToWait = httpTimeToWait * 2;
-            }, httpTimeToWait);
-            return;
-        }
-        
-        res.on('data', chunk => {
-            console.log('received data....');
-            lastKeepAlive = new Date();
-            try {
-                print(chunk);
-            } catch(err) {
-                console.log('*******Keep Alive*******');
-                console.log(new Date());
-            }       
-        });
-
-        res.on('error', e => {
-            if (e.code === 'ECONNRESET') {
-                console.log('tcp/ip level error: restart connection');
-                setTimeout(() => {
-                    connectToStream();
-                    tcpTimeToWait += 250; 
-                }, tcpTimeToWait);
-            } else {
-                console.error(e);
+(async () => {
+    try {
+        result = await AreaDesc.find({}).populate('polygon');
+        const areaDescriptions = result.map(entry => {
+            const coords = [];
+            for (const coord of entry.polygon.coordinates) {
+                const c1 = coord[0].toString();
+                const c2 = coord[1].toString();
+                coords.push([Number(c1),Number(c2)]);
+            }
+            return {
+                desc: entry.desc,
+                polygon: {
+                    area: entry.polygon.area,
+                    coordinates: coords
+                }
             }
         });
-    });
-
-    req.on('error', e => {
-        console.log('system level error: restart connection');
-        setTimeout(() => {
-            connectToStream();
-            tcpTimeToWait += 250; 
-        }, tcpTimeToWait);
-    });
+        startServer(areaDescriptions);
+    } catch(err) {
+        console.error(err);
+    }
 })();
 
-const print = (chunk) => {
-    const tweet = JSON.parse(chunk);
-    console.log(tweet);
-    console.log(tweet.data.text + ' id: '+tweet.data.id);
+const processTweet = (chunk, patterns, areaDescriptions) => {
+        const tweet = JSON.parse(chunk);
+        console.log('received tweet: '+tweet.data.text + ' id: '+tweet.data.id);
+        const match = search_tweet(tweet.data.text.toLowerCase(), patterns);
+        console.log(`match is: `, match);
+        try {
+            if (match < 0) return;
+            console.log(`generating random point in ${patterns[match]}`);
+            const coords = areaDescriptions[match].polygon.coordinates;
+            const r = random_point(coords);
+            console.log(`random point: ${r}`);
+            console.log(`adding new feature to db`);
+            const geometry = new Geometry('Point', r);
+            const properties = new Properties(tweet.data.text, new Date(), patterns[match]);
+            const details = {type: 'Feature', geometry, properties};
+            const feature = new Feature(details);
+            feature.save(err => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log(`New feature added to db`);
+            });
+        } catch(err) {
+            console.error(err);
+        }
 };
+
+const startServer = (areaDescriptions) => {
+    let patterns = []; //area name patterns e.g. 'buru', 'buru buru'...
+    for (const ad of areaDescriptions) {
+        patterns.push(ad.desc);
+    }
+    let httpTimeToWait = 30000;
+    let tcpTimeToWait = 0;
+    let timeout = 30000;
+
+    const options = {
+        hostname: 'api.twitter.com',
+        port: 443,
+        path: '/2/tweets/search/stream',
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer '+process.env.TWITTER_BEARER_TOKEN, 'Content-Type': 'application/json' },
+    };
+
+    function connectToStream() {
+        req = https.get(options, (res) => {
+            console.log('statusCode:', res.statusCode);
+            console.log('number of sockets:', https.globalAgent.sockets[Object.keys(https.globalAgent.sockets)[0]].length);
+            if (res.statusCode !== 200) {
+                console.log(new Date() + "http level error: restart connection");
+                setTimeout(() => {
+                    req.destroy();
+                    connectToStream(); //try removing this
+                    httpTimeToWait = httpTimeToWait * 2;
+                    return;
+                }, httpTimeToWait);
+                return;
+            }
+
+            httpTimeToWait = 30000;
+            tcpTimeToWait = 0;
+
+            let reqTimeout = setTimeout(() => {
+                  console.log('No data received for 30 seconds. Closing connection.');
+                  req.destroy();
+                }, timeout);
+            
+            res.on('data', chunk => {
+                console.log('number of sockets:', https.globalAgent.sockets[Object.keys(https.globalAgent.sockets)[0]].length);
+                lastKeepAlive = new Date();
+                try {
+                    processTweet(chunk, patterns, areaDescriptions);
+                } catch(err) {
+                    console.log('*******Keep Alive*******');
+                    console.log(new Date());
+                }       
+                clearTimeout(reqTimeout);
+                reqTimeout = setTimeout(() => {
+                  console.log('No data received for 30 seconds. Closing connection.');
+                  req.destroy();
+                }, timeout);
+            });
+
+            res.on('end', () => {
+                console.log('response has ended.');
+            })
+
+            res.on('error', e => {
+                if (e.code === 'ECONNRESET') {
+                    console.log('tcp/ip level error: restart connection');
+                    setTimeout(() => {
+                        req.destroy();
+                        connectToStream();
+                        tcpTimeToWait += 250; 
+                    }, tcpTimeToWait);
+                } else {
+                    console.error(e);
+                }
+            });
+        });
+
+        req.on('error', e => {
+            console.log('system level error: restart connection');
+            setTimeout(() => {
+                req.destroy();
+                connectToStream();
+                tcpTimeToWait += 250; 
+            }, tcpTimeToWait);
+        });
+    };
+    connectToStream();
+}
+
