2782b42654dbfca768812b5cc1dedeb19453a06f
Eric Githinji <ericgithinji@gmail.com>
2022-12-27 12:48:53 +0300

    Add today_stats functionality.

--- controllers/featuresController.js
+++ controllers/featuresController.js
@@ -1,4 +1,5 @@
-const Feature = require("../models/feature");
+const Feature = require('../models/feature');
+const Stats = require('../models/objects/stats');
 
 exports.today_features = (req, res, next) => {
     const today = new Date();
@@ -7,7 +8,7 @@ exports.today_features = (req, res, next) => {
     Feature.find({ "properties.posted_on":  { $gte: startOfToday, $lt: endOfToday } })
     .exec((err, features) => {
         if (err) {
-            console.log("Error getting features.");
+            console.log("Error retreiving features.");
             return  next(err);
         }
         return res.json(features);
@@ -15,5 +16,29 @@ exports.today_features = (req, res, next) => {
 };
 
 exports.today_stats = (req, res) => {
-    res.send("NOT IMPLEMENTED: today stats");
+    const today = new Date();
+    const startOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const endOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+    Feature.find({ "properties.posted_on":  { $gte: startOfToday, $lt: endOfToday } })
+    .exec((err, features) => {
+        if (err) {
+            console.log("Error retreiving features.");
+            return  next(err);
+        }
+        const top = {};
+        let total = 0;
+        for (const feature of features) {
+            top[feature.properties.area] = (top[feature.properties.area] || 0) + 1;
+            total += 1;
+        }
+        const topOutages = [];
+        for (const area in top) {
+            topOutages.push({area, count: top[area]});
+        }
+        topOutages.sort((a,b) => b.count - a.count);
+        const top3 = [topOutages[0], topOutages[1], topOutages[2]];
+
+        const stats = new Stats(top3, total);
+        return res.json(stats);
+    });
 };
---
+++ models/objects/stats.js
@@ -0,0 +1,6 @@
+module.exports = class Stats {
+    constructor(top3, todayTotal) {
+        this.top3 = top3;
+        this.todayTotal = todayTotal;
+    }
+}
