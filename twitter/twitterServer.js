076e3ecadc02569ccc252f50fa45408194656142
Eric Githinji <ericgithinji@gmail.com>
2022-12-30 19:19:03 +0300

    Emit event when new feature added to db.

--- twitter/twitterServer.js
+++ twitter/twitterServer.js
@@ -6,10 +6,16 @@ const Polygon = require('../models/polygon');
 const Feature = require('../models/feature');
 const { search_tweet, random_point } = require('../rust-modules/pkg');
 require('dotenv').config();
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+const debug = require("debug")("twitterServer");
 
 (async () => {
-    try {
-        result = await AreaDesc.find({}).populate('polygon');
+    result = AreaDesc.find({}).populate('polygon')
+    .exec((err, result) => {
+        if (err){
+            debug(`error retreiving area descriptions: ${err}`);
+        }
         const areaDescriptions = result.map(entry => {
             const coords = [];
             for (const coord of entry.polygon.coordinates) {
@@ -26,36 +32,35 @@ require('dotenv').config();
             }
         });
         startServer(areaDescriptions);
-    } catch(err) {
-        console.error(err);
-    }
+    });
 })();
 
 const processTweet = (chunk, patterns, areaDescriptions) => {
         const tweet = JSON.parse(chunk);
-        console.log('received tweet: '+tweet.data.text + ' id: '+tweet.data.id);
+        debug(`received tweet: ${tweet.data.text} id: ${tweet.data.id}`);
         const match = search_tweet(tweet.data.text.toLowerCase(), patterns);
-        console.log(`match is: `, match);
+        debug(`match is: ${match}`);
         try {
             if (match < 0) return;
-            console.log(`generating random point in ${patterns[match]}`);
+            debug(`generating random point in ${patterns[match]}`);
             const coords = areaDescriptions[match].polygon.coordinates;
             const r = random_point(coords);
-            console.log(`random point: ${r}`);
-            console.log(`adding new feature to db`);
+            debug(`random point: ${r}`);
+            debug(`adding new feature to db`);
             const geometry = new Geometry('Point', r);
             const properties = new Properties(tweet.data.text, new Date(), patterns[match]);
             const details = {type: 'Feature', geometry, properties};
             const feature = new Feature(details);
             feature.save(err => {
                 if (err) {
-                    console.error(err);
+                    debug(`error saving feature in db: ${err}`);
                     return;
                 }
-                console.log(`New feature added to db`);
+                debug(`New feature added to db. Notify clients.`);
+                emitter.emit('new-feature'); 
             });
         } catch(err) {
-            console.error(err);
+            debug(`error processing tweet: ${err}`);
         }
 };
 
@@ -66,25 +71,26 @@ const startServer = (areaDescriptions) => {
     }
     let httpTimeToWait = 30000;
     let tcpTimeToWait = 0;
-    let timeout = 30000;
+    let timeout = 240000;
 
     const options = {
         hostname: 'api.twitter.com',
         port: 443,
         path: '/2/tweets/search/stream',
         method: 'GET',
-        headers: { 'Authorization': 'Bearer '+process.env.TWITTER_BEARER_TOKEN, 'Content-Type': 'application/json' },
+        headers: { 'Authorization': 'Bearer '+process.env.TWITTER_BEARER_TOKEN, 'Content-Type': 'application/json', 'Connection': 'Keep-Alive'},
     };
 
     function connectToStream() {
-        req = https.get(options, (res) => {
-            console.log('statusCode:', res.statusCode);
-            console.log('number of sockets:', https.globalAgent.sockets[Object.keys(https.globalAgent.sockets)[0]].length);
+        stream = https.get(options, (res) => {
+            emitter.emit('server-running');
+            debug(`statusCode: ${res.statusCode} next reset: ${res.headers['x-rate-limit-reset']}`);
+            debug(`number of sockets: ${https.globalAgent.sockets[Object.keys(https.globalAgent.sockets)[0]].length}`);
             if (res.statusCode !== 200) {
-                console.log(new Date() + "http level error: restart connection");
+                debug(`${new Date()} http level error: restart connection`);
                 setTimeout(() => {
-                    req.destroy();
-                    connectToStream(); //try removing this
+                    stream.destroy();
+                    connectToStream();
                     httpTimeToWait = httpTimeToWait * 2;
                     return;
                 }, httpTimeToWait);
@@ -95,48 +101,47 @@ const startServer = (areaDescriptions) => {
             tcpTimeToWait = 0;
 
             let reqTimeout = setTimeout(() => {
-                  console.log('No data received for 30 seconds. Closing connection.');
-                  req.destroy();
+                  debug(`No data received for 240 seconds. Closing connection.`);
+                  stream.destroy();
                 }, timeout);
             
             res.on('data', chunk => {
-                console.log('number of sockets:', https.globalAgent.sockets[Object.keys(https.globalAgent.sockets)[0]].length);
-                lastKeepAlive = new Date();
+                debug(`number of sockets: ${https.globalAgent.sockets[Object.keys(https.globalAgent.sockets)[0]].length}`);
                 try {
                     processTweet(chunk, patterns, areaDescriptions);
                 } catch(err) {
-                    console.log('*******Keep Alive*******');
-                    console.log(new Date());
+                    debug(`*******Keep Alive*******`);
+                    debug(`${new Date()}`);
                 }       
                 clearTimeout(reqTimeout);
                 reqTimeout = setTimeout(() => {
-                  console.log('No data received for 30 seconds. Closing connection.');
-                  req.destroy();
+                  debug(`No data received for 240 seconds. Closing connection.`);
+                  stream.destroy();
                 }, timeout);
             });
 
             res.on('end', () => {
-                console.log('response has ended.');
+                debug(`response has ended.`);
             })
 
             res.on('error', e => {
                 if (e.code === 'ECONNRESET') {
-                    console.log('tcp/ip level error: restart connection');
+                    debug(`tcp/ip level error: restart connection`);
                     setTimeout(() => {
-                        req.destroy();
+                        stream.destroy();
                         connectToStream();
                         tcpTimeToWait += 250; 
                     }, tcpTimeToWait);
                 } else {
-                    console.error(e);
+                    debug(`error: ${e}`);
                 }
             });
         });
 
-        req.on('error', e => {
-            console.log('system level error: restart connection');
+        stream.on('error', e => {
+            debug(`system level error: restart connection`);
             setTimeout(() => {
-                req.destroy();
+                stream.destroy();
                 connectToStream();
                 tcpTimeToWait += 250; 
             }, tcpTimeToWait);
@@ -145,3 +150,4 @@ const startServer = (areaDescriptions) => {
     connectToStream();
 }
 
+module.exports = emitter;
