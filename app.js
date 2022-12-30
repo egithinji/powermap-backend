9575a5402de4b97b829c2be25bb19432a533d5e3
Eric Githinji <ericgithinji@gmail.com>
2022-12-30 20:10:02 +0300

    rename mongo env variable.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 const dev_db_url = "mongodb+srv://library:ZEO5uzfoHOvpsE6n@cluster0.qkhblew.mongodb.net/powermap?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGO_DB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
