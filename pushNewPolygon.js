3ddcbbcaff65cb28100509495946dec852a037e9
Eric Githinji <ericgithinji@gmail.com>
2023-02-07 10:47:24 +0300

    Use execSync and add some logging.

--- pushNewPolygon.js
+++ pushNewPolygon.js
@@ -1,17 +1,18 @@
 // Called when a new polygon is entered via the form in the front end
 // Creates a PR containing the new polygon
 
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 require('dotenv').config();
 
 const pushNewPolygon = () => {
     try {
-        exec(`git clone https://${process.env.GITHUB_TOKEN}@github.com/egithinji/powermap-backend`);
-        exec('git checkout -b newpolygon');
-        exec('git add newPolygon.json');
-        exec('git commit -m "From script: New polygon."');
-        exec(`git remote add origin https://${process.env.GITHUB_TOKEN}@github.com/egithinji/powermap-backend`);
-        exec(`git push https://${process.env.GITHUB_TOKEN}@github.com/egithinji/powermap-backend.git`);
+        console.log(execSync(`git clone https://${process.env.GITHUB_TOKEN}@github.com/egithinji/powermap-backend`).toString());
+        console.log(`after cloning, git status output is: ${execSync('git status').toString()}`);
+        execSync('git checkout -b newpolygon');
+        execSync('git add newPolygon.json');
+        execSync('git commit -m "From script: New polygon."');
+        execSync(`git remote add origin https://${process.env.GITHUB_TOKEN}@github.com/egithinji/powermap-backend`);
+        execSync(`git push https://${process.env.GITHUB_TOKEN}@github.com/egithinji/powermap-backend.git`);
         console.log('finished git commands');
     } catch (err) {
         console.error(`Error pushing new polygon: ${err}`);
